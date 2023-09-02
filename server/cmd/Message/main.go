b77859446355141c6322c782521d221a642068cf
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-09-02 15:13:52 +0100

    feat: basic server setup

--- server/cmd/Message/main.go
+++ server/cmd/Message/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Index)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello, world!"))
+	if err != nil {
+		log.Printf("Could not write response: %s\n", err.Error())
+	}
 }
