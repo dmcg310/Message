2e9a37355c1f01b1f68e247b5a050ec7e1a17de4
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-09-20 22:59:49 +0100

    fix: host & port added for db connection

--- server/internal/database/database.go
+++ server/internal/database/database.go
@@ -9,7 +9,7 @@ import (
 	"os"
 )
 
-func LoadEnv() (DBNAME, USER, PASSWORD string) {
+func LoadEnv() (DBNAME, USER, PASSWORD, HOST, PORT string) {
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println("No .env file found, looking for config vars...")
@@ -18,18 +18,20 @@ func LoadEnv() (DBNAME, USER, PASSWORD string) {
 	DBNAME = os.Getenv("POSTGRES_DB")
 	USER = os.Getenv("POSTGRES_USER")
 	PASSWORD = os.Getenv("POSTGRES_PASSWORD")
+	HOST = os.Getenv("POSTGRES_HOST")
+	PORT = os.Getenv("POSTGRES_PORT")
 
 	if DBNAME == "" || USER == "" || PASSWORD == "" {
 		log.Fatalf("Failed to load database config vars")
 	}
 
-	return DBNAME, USER, PASSWORD
+	return DBNAME, USER, PASSWORD, HOST, PORT
 }
 
 func InitDatabase() *sql.DB {
-	DBNAME, USER, PASSWORD := LoadEnv()
-	conn := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		USER, PASSWORD, DBNAME)
+	DBNAME, USER, PASSWORD, HOST, PORT := LoadEnv()
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		HOST, PORT, USER, PASSWORD, DBNAME)
 
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
